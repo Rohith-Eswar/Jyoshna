5d081e11848feaeb1ff76ccc2e76d41381191296
Rohith <eswarrohith238@gmail.com>
2025-02-05 09:26:08 +0530

    commit need to kill

--- shellscript.sh
+++ shellscript.sh
@@ -1,3 +1,11 @@
-# yem bin!bash
-#echo
-# files need to be added
+#!/bib/bash
+#Author:Rohith
+#date created:Feb5
+#Description:Learning stage of script
+#Date modified:
+#purpose to modify:
+
+
+
+
+echo 'end'
